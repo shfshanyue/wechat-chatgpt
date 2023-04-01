14d80797538f4438f675d6f37910d415854f77f7
shfshanyue <xianger94@gmail.com>
2023-04-02 01:50:12 +0800

    fix: base_url error

--- config.ts
+++ config.ts
@@ -17,7 +17,7 @@ export default {
   // 自动同意添加好友的口令
   acceptText: /芝麻开门/,
 
-  baseURL: process.env.BASE_URL || 'https://api.openai.com/v1',
+  baseURL: process.env.BASE_URL || 'https://api.openai.com',
   apiKey: process.env.OPEN_API_KEY.split(','),
   model: process.env.GPT_MODEL || 'gpt-3.5-turbo',
   prompt: process.env.PROMPT || '',
