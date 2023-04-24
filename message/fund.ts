8b204baae940cc39741f801af05d900e6f02be92
shfshanyue <xianger94@gmail.com>
2023-04-24 11:36:35 +0800

    chore: 整理 dependencies

--- message/fund.ts
+++ message/fund.ts
@@ -1,14 +1,13 @@
-import axios from 'axios'
+// import wretch from 'wretch'
 
-const url = 'http://fund.eastmoney.com/data/FundGuideapi.aspx?dt=0&rs=1y,100_1n,100_3n,100_2n,100'
+// const url = 'http://fund.eastmoney.com/data/FundGuideapi.aspx?dt=0&rs=1y,100_1n,100_3n,100_2n,100'
 
-function getFunds () {
-  return axios.get(url).then(({ data }) => {
-    return JSON.parse(data.slice(14)).datas.map(x => x.split(','))
-  })
-}
+// function getFunds () {
+//   return wretch(url).json(({ data }) => {
+//     return JSON.parse(data.slice(14)).datas.map(x => x.split(','))
+//   })
+// }
 
 export async function handle (): Promise<string> {
-  const funds = await getFunds()
-  return funds.map((fund: string[]) => `${fund[0]}: ${fund[1]}`).join('\n')
+  return 'it worked'
 }
